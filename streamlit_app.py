9157962af19ca8fd8560fdc6f1d84592b8da29ca
nazlan09 <rendinazlan921@gmail.com>
2025-04-24 13:44:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("naze")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.https://github.com/nazlan09/nazlan/tree/main/views
