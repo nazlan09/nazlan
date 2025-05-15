9cb96ceade7a35180760559704bbef87d1d8d529
nazlan09 <rendinazlan921@gmail.com>
2025-05-15 14:58:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 
-import streamlit as st
-
 st.title("Hiii wllcome di situs nazlan")
 st.image("IMG_6843.jpeg",width=150)
 st.write("\n")
 st.subheader("Nazlan D")
 st.write("Mari Belajar Bersama  Yesz!")
-st.https://github.com/nazlan09/nazlan/tree/main/views
